4d5fdd7cd27d1af1815c51a914fd507867433a1f
Pratik Mohite <pratik.mohite@atyeti.com>
2025-07-20 21:59:40 +0530

    Day2 practice

---
+++ configRules.ts
@@ -0,0 +1,12 @@
+/*
+
+list down the config rules are available in the configRules.ts file
+- no-unused-vars: This rule disallows unused variables in the code. 
+- no-console: This rule disallows the use of console statements in the code.
+- no-debugger: This rule disallows the use of debugger statements in the code.
+- no-undef: This rule disallows the use of undeclared variables in the code.
+- no-redeclare: This rule disallows the redeclaration of variables in the code. 
+- no-shadow: This rule disallows variable declarations that shadow variables declared in the outer scope.
+- no-var: This rule disallows the use of var keyword in the code.
+- prefer-const: This rule enforces the use of const keyword for variables that are never reassigned.
+*/
